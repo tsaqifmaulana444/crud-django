28bdb42191c5d214dc7328c08bf1537f17715f2a
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2022-12-28 19:03:06 +0700

    security

--- perpus/settings.py
+++ perpus/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&@&8sqt$5)30!(nz%_tos-+d2#7ixhb!_*q4q%norlzxk_%!hb'
+# SECRET_KEY = 'django-insecure-&@&8sqt$5)30!(nz%_tos-+d2#7ixhb!_*q4q%norlzxk_%!hb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
